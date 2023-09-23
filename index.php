4b87c8e29d72e3c1d92d966890874ea69c5894ca
gerald <ger23z@gmail.com>
2023-09-23 19:15:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<html>
-<head>
-</head>
-<body>
 
-<center><a href="http://ger23.free.fr/articles" target="blank">PlAY THE GAME</a></center>
-<center><a href="http://ger23.free.fr/articles/appcenter" target="blank">PlAY THE GAME</a></center>
-
-
-</body>
-</html>
